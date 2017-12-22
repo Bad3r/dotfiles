9ea625ffe9198b441570367f715e1408943c2965
bd3r <bad3r@protonmail.com>
2017-12-22 13:43:16 -0500

    added a script for xrandar that fixes a problem i have with my monitor

---
+++ .dorm_randr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --right-of eDP1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
