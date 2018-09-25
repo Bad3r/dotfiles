a5c766b4c4ca5bf761e87ef2d6738da45d61d10b
bd3r <bad3r@protonmail.com>
2018-09-25 09:03:32 -0400

    added aliases for Git

--- .alias.zsh
+++ .alias.zsh
@@ -44,3 +44,30 @@ alias tb="nc termbin.com 9999"			# Upload files to netcat-based pastebin.
 alias hex="hyx"					# CLI hex editor
 alias fetch="neofetch"				# Show system information.
 
+
+# Git :
+
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
+alias gbv='git branch | nvim -MR -'
+alias gc='git commit'
+alias gch='git checkout'
+alias gchp='git checkout -p'
+alias gcm='git commit -m'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gdn='git diff --name-only'
+alias gdv='git diff | nvim -MR -'
+alias gl='git log'
+alias gla='git log --author='
+alias glf='git log --follow --'
+alias glv='git log | nvim -MR -'
+alias groot='cd $(git rev-parse --show-toplevel)'
+alias gs='git status'
+alias gsh='git show'
+alias gsroot='cd $(git rev-parse --show-superproject-working-tree)'
+alias gst='git stash'
+
+
+
