396f3235c34481ba4df6a73139f95eff18ecc16b
Bad3r <bad3r@protonmail.com>
2021-04-29 18:33:40 -0400

    yay lua?

--- config/awesome/config/settings.lua
+++
@@ -1,8 +0,0 @@
-local settings                  = {}
-
-settings.wlan_interface         = "wlan0"
-settings.lan_interface          = "enp0s31f6"
-settings.is_bluetooth_presence  = true
-settings.script_dir             = "$HOME/.config/awesome/scripts/"
-
-return settings
