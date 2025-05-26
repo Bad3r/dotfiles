6dd25292955395bda7fb907cec2a464c3a162ab9
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-05-26 10:14:08 +0300

    enhance: revert back to ol' trust worthy Antibody

--- config/zsh/zshrc.d/03-plugins.zsh
+++ config/zsh/zshrc.d/03-plugins.zsh
@@ -2,24 +2,34 @@
 # if antibody is installed; bundle zsh plugins
 
 # TODO: replace with a mantained plugin manager
-if ! command -v antidote &> /dev/null; then
+if ! command -v antibody &> /dev/null; then
     return
 fi 
 
-if hash antidote 2>/dev/null; then
-    antidote load <<-plugins
+if hash antibody 2>/dev/null; then
+    source <(antibody init)
+    antibody bundle <<-plugins
 
-    # https://github.com/zsh-users/zsh-syntax-highlighting
-    zsh-users/zsh-syntax-highlighting
+    # https://github.com/mattmc3/ez-compinit
+    mattmc3/ez-compinit
 
-    # https://github.com/zsh-users/zsh-autosuggestions
-    zsh-users/zsh-autosuggestions
+    # https://github.com/zsh-users/zsh-completions
+    zsh-users/zsh-completions kind:fpath path:src
 
     # https://github.com/Aloxaf/fzf-tab
     Aloxaf/fzf-tab
 
-    #  https://github.com/wfxr/forgit
-    # wfxr/forgit
+    # https://github.com/mattmc3/zfunctions
+    mattmc3/zfunctions
+
+    # https://github.com/zsh-users/zsh-autosuggestions
+    zsh-users/zsh-autosuggestions
+
+    # https://github.com/zdharma-continuum/fast-syntax-highlighting
+    zdharma-continuum/fast-syntax-highlighting kind:defer
+
+    # https://github.com/zsh-users/zsh-history-substring-search
+    zsh-users/zsh-history-substring-search
 
     #  https://github.com/evanthegrayt/vagrant-box-wrapper
     evanthegrayt/vagrant-box-wrapper
@@ -27,9 +37,6 @@ if hash antidote 2>/dev/null; then
     # https://github.com/NullSense/fuzzy-sys
     # Utility for using systemctl interactively via junegunn/fzf.
     NullSense/fuzzy-sys
-
-    # https://github.com/zsh-users/zsh-history-substring-search
-    zsh-users/zsh-history-substring-search
 
    # https://github.com/g-plane/pnpm-shell-completion
     g-plane/pnpm-shell-completion
@@ -41,6 +48,9 @@ if hash antidote 2>/dev/null; then
     # https://github.com/wfxr/forgit
     wfxr/forgit
 
-plugins
+    # NOTE: Replaced with starship prompt (seperate package)
+    # https://github.com/sindresorhus/pure
+    # sindresorhus/pure     kind:fpath
 
+plugins
 fi
