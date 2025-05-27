f503cfeb9a6fe34ab33f1b1d16b4104d516295da
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-05-27 09:39:24 +0300

    enhance: Replace Antigen with Antidote for zsh plugin management

--- config/zsh/zshrc.d/00-completion.zsh
+++ config/zsh/zshrc.d/00-completion.zsh
@@ -1,58 +1,58 @@
 
-# Completion
-#
-# do not autoselect the first completion entry
-unsetopt menu_complete   
+# # Completion
+# #
+# # do not autoselect the first completion entry
+# unsetopt menu_complete   
 
-# 20.2.1 Use of compinit
-# https://zsh.sourceforge.io/Doc/Release/Completion-System.html#Use-of-compinit
+# # 20.2.1 Use of compinit
+# # https://zsh.sourceforge.io/Doc/Release/Completion-System.html#Use-of-compinit
 
-autoload -Uz compinit
+# autoload -Uz compinit
 
-# Add custom completion directory to fpath
-fpath=("${ZSH_CONF_DIR}/completion.d" $fpath)
+# # Add custom completion directory to fpath
+# fpath=("${ZSH_CONF_DIR}/completion.d" $fpath)
 
-# set compinit dump file locaton
-compinit -d "${ZSH_CONF_DIR:-$HOME/.config/zsh}/completion.d/.zshcompdump"
+# # set compinit dump file locaton
+# compinit -d "${ZSH_CONF_DIR:-$HOME/.config/zsh}/completion.d/.zshcompdump"
 
-# Case-insensitive and hyphen insensitive path-completion 
-CASE_SENSITIVE=false
-HYPHEN_INSENSITIVE=true 
+# # Case-insensitive and hyphen insensitive path-completion 
+# CASE_SENSITIVE=false
+# HYPHEN_INSENSITIVE=true 
 
-if [[ "$CASE_SENSITIVE" = true ]]; then
-  zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-else
-  if [[ "$HYPHEN_INSENSITIVE" = true ]]; then
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
-  else
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-  fi
-fi
-unset CASE_SENSITIVE HYPHEN_INSENSITIVE
+# if [[ "$CASE_SENSITIVE" = true ]]; then
+#   zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
+# else
+#   if [[ "$HYPHEN_INSENSITIVE" = true ]]; then
+#     zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+#   else
+#     zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+#   fi
+# fi
+# unset CASE_SENSITIVE HYPHEN_INSENSITIVE
 
-# Complete . and .. special directories
-zstyle ':completion:*' special-dirs true
+# # Complete . and .. special directories
+# zstyle ':completion:*' special-dirs true
 
-# disable named-directories autocompletion
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+# # disable named-directories autocompletion
+# zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
-# Use caching so that commands like apt and dpkg complete are useable
-zstyle ':completion:*' use-cache yes
-zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
+# # Use caching so that commands like apt and dpkg complete are useable
+# zstyle ':completion:*' use-cache yes
+# zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
 
-# Don't complete uninteresting users
-zstyle ':completion:*:*:*:users' ignored-patterns \
-        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
-        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
-        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
-        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
-        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
-        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
-        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
-        usbmux uucp vcsa wwwrun xfs '_*'
+# # Don't complete uninteresting users
+# zstyle ':completion:*:*:*:users' ignored-patterns \
+#         adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+#         clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+#         gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+#         ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+#         named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+#         operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+#         rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+#         usbmux uucp vcsa wwwrun xfs '_*'
 
-# ... unless we really want to.
-zstyle '*' single-ignored show
+# # ... unless we really want to.
+# zstyle '*' single-ignored show
 
 
 
--- config/zsh/zshrc.d/03-plugins.zsh
+++ config/zsh/zshrc.d/03-plugins.zsh
@@ -1,11 +1,14 @@
-# list of plugins
-# if antibody is installed; bundle zsh plugins
+# Zsh Plugins Configuration File
 
-# TODO: replace with a mantained plugin manager
-if hash antibody 2>/dev/null; then
-    source <(antibody init)
-    antibody bundle <<-plugins
+export ANTIDOTE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}/antidote
+PLUGINS_CONF_FILE="${${(%):-%N}:A}" # this file
+PLUGINS_DST_FILE="$ANTIDOTE_HOME/plugins.zsh"
 
+
+if [[ ! "$PLUGINS_DST_FILE" -nt "$PLUGINS_CONF_FILE" ]]; then
+    source "$DOTFILES/.antidote/antidote.zsh"
+    mkdir -p "${PLUGINS_CONF_FILE:h}"
+    antidote bundle <<-plugins >| "$PLUGINS_DST_FILE"
     # https://github.com/mattmc3/ez-compinit
     mattmc3/ez-compinit
 
@@ -28,14 +31,14 @@ if hash antibody 2>/dev/null; then
     zsh-users/zsh-history-substring-search
 
     #  https://github.com/evanthegrayt/vagrant-box-wrapper
-    evanthegrayt/vagrant-box-wrapper
+    # evanthegrayt/vagrant-box-wrapper
 
     # https://github.com/NullSense/fuzzy-sys
     # Utility for using systemctl interactively via junegunn/fzf.
-    NullSense/fuzzy-sys
+    NullSense/fuzzy-sys # command: $ fuzzy-sys
 
-   # https://github.com/g-plane/pnpm-shell-completion
-    g-plane/pnpm-shell-completion
+    # https://github.com/g-plane/pnpm-shell-completion
+    # g-plane/pnpm-shell-completion
 
     # https://github.com/hlissner/zsh-autopair
     hlissner/zsh-autopair
@@ -47,6 +50,11 @@ if hash antibody 2>/dev/null; then
     # NOTE: Replaced with starship prompt (seperate package)
     # https://github.com/sindresorhus/pure
     # sindresorhus/pure     kind:fpath
-
 plugins
 fi
+
+source "$PLUGINS_DST_FILE"
+
+# Set path to completion dump file
+# Must be after loading mattmc3/ez-compinit
+compinit -d "${ZSH_CONF_DIR:-$HOME/.config/zsh}/completion.d/.zshcompdump"
