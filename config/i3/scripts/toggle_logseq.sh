3ce326156eee9b3e773e4fd63be17ac0033f65ea
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-10-01 19:57:57 +0300

    home-manager: refresh stylix symlinks

--- config/i3/scripts/toggle_logseq.sh
+++ config/i3/scripts/toggle_logseq.sh
@@ -1,17 +1,1 @@
-#!/bin/sh
-
-# source window utils lib to calculate window size and position
-. "$USR_LIB_DIR/window_utils"
-
-# Calculate the window geometry
-calculate_window_geometry
-
-# Check if Logseq is running
-if ! pgrep -f logseq >/dev/null; then
-    notify-send "Info" "Starting Logseq.."
-    i3-msg "exec --no-startup-id i3-scratchpad-show-or-create 'Logseq' 'logseq'"
-    sleep 30
-fi
-
-# Run the i3 command with the calculated position and size
-i3-msg "[class=\"Logseq\"] scratchpad show, move position ${TARGET_X}px ${TARGET_Y}px, resize set ${TARGET_WIDTH}px ${TARGET_HEIGHT}px"
+/nix/store/9a47w7h9y8njx7kyxmhmw74wxhs2k30j-home-manager-files/.config/i3/scripts/toggle_logseq.sh
